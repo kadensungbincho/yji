8bdade3c721fcea2ba295cb9f006e7c8783877ed
kadensungbincho <kaden.cho@balancehero.com>
2019-07-08 20:53:47 +0900

    Download the files through shell script

---
+++ download_data.sh
@@ -0,0 +1,23 @@
+
+listings="http://data.insideairbnb.com/united-states/ny/new-york-city/2019-06-02/data/listings.csv.gz"
+calendar="http://data.insideairbnb.com/united-states/ny/new-york-city/2019-06-02/data/calendar.csv.gz"
+review="http://data.insideairbnb.com/united-states/ny/new-york-city/2019-06-02/data/reviews.csv.gz"
+neighbor="http://data.insideairbnb.com/united-states/ny/new-york-city/2019-06-02/visualisations/neighbourhoods.csv"
+neighbor_geo="http://data.insideairbnb.com/united-states/ny/new-york-city/2019-06-02/visualisations/neighbourhoods.geojson"
+
+data_dir="data"
+targets=($listings $calendar $review $neighbor $neighbor_geo)
+
+for i in "${targets[@]}";
+do
+    name=$(echo $i | rev | cut -d/ -f1 | rev)
+    echo downloading $name ...
+    wget $i -O $data_dir/$name
+done
+
+echo gunzipping...
+for file in "$data_dir"/*; do
+  if [ ${file: -3} == ".gz" ]; then
+    gunzip $file
+  fi
+done
