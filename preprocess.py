37558c5bd1b649ae14d029faf7de3dbb7657a554
kadensungbincho <kaden.cho@balancehero.com>
2019-07-12 01:19:29 +0900

    Success python file for spark-submit

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,6 @@
+import sys
 import re
+from datetime import datetime
 
 import pyspark.sql.functions as F
 from h3 import h3
@@ -6,7 +8,22 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import DoubleType, IntegerType, StringType, StructType
 
 
-spark = SparkSession.builder.getOrCreate()
+DATE_FORMAT = '%Y-%m-%d'
+
+
+try:
+    datetime.strptime(sys.argv[1], DATE_FORMAT)
+    execution_date = sys.argv[1]
+except:
+    raise ValueError(f'Please specify data arguments with format - {DATE_FORMAT}')
+
+
+spark = (
+    SparkSession
+    .builder
+    .config("hive.metastore.client.factory.class", "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory")
+    .enableHiveSupport()
+    .getOrCreate())
 
 SCHEMA = {
     "host_id": IntegerType,
@@ -97,8 +114,9 @@ udf_geo_to_h3 = F.udf(geo_to_h3, StringType())
 # 4, 22
 # 6, 3
 # 7, 1.2
-# 8, 0.5
-for i in [4, 6, 7, 8]:
+# 9, 0.17
+resolutions = [4, 6, 7, 9]
+for i in resolutions:
     df = df.withColumn(f'h3_r{i}', udf_geo_to_h3(df.latitude, df.longitude, F.lit(i)))
 
 
@@ -115,8 +133,9 @@ def get_agg_df(key, suffix):
     df = spark.sql(f"""
     SELECT
         {key}
-        , property_type
-        , room_type
+        -- , property_type
+        -- , room_type
+        , count(*) as cnt_{suffix}
         , {get_columns('avg', ', ', suffix)}
     FROM source
     GROUP BY 1
@@ -125,7 +144,37 @@ def get_agg_df(key, suffix):
 
 ngbg_df = get_agg_df('neighbourhood_group_cleansed', 'ngbg')
 ngb_df = get_agg_df('neighbourhood', 'ngb')
-h3_r3_df = get_agg_df('h3_r3', 'h3_r3')
+h3_r4_df = get_agg_df('h3_r4', 'h3_r4')
 h3_r6_df = get_agg_df('h3_r6', 'h3_r6')
 h3_r7_df = get_agg_df('h3_r7', 'h3_r7')
-h3_r7_df = get_agg_df('h3_r8', 'h3_r8')
+h3_r9_df = get_agg_df('h3_r9', 'h3_r9')
+
+
+dfs = [ngbg_df, ngb_df, h3_r4_df, h3_r6_df, h3_r7_df, h3_r9_df]
+keys = ['neighbourhood_group_cleansed', 'neighbourhood', *[f'h3_r{r}' for r in resolutions]]
+
+left_df = spark.sql("""
+SELECT 
+    host_id
+    , neighbourhood_group_cleansed
+    , neighbourhood
+    , h3_r4
+    , h3_r6
+    , h3_r7
+    , h3_r9
+FROM source
+GROUP BY 1, 2, 3, 4, 5, 6, 7
+""")
+
+
+def left_joinner(left, right, key):
+    return left.join(right, key, how='left')
+
+
+for _df, _key in zip(dfs, keys):
+    left_df = left_joinner(left_df, _df, _key)
+
+left_df = left_df.withColumn('dt', F.lit(execution_date))
+
+# Save To S3
+left_df.repartition(10).write.parquet(f's3://yji/db/listings_info/dt={execution_date}')
